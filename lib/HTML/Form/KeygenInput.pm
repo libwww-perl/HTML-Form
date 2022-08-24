cffb9e7e7f056c67fa2703062d52a3c71ac73b44
Julien Fiegehenn <simbabque@cpan.org>
2022-08-24 22:12:58 +0100

    split packages into their own files

---
+++ lib/HTML/Form/KeygenInput.pm
@@ -0,0 +1,20 @@
+package HTML::Form::KeygenInput;
+
+use strict;
+use parent 'HTML::Form::Input';
+
+our $VERSION = '6.11';
+
+# ABSTRACT: An HTML form keygen input element for use with HTML::Form
+
+sub challenge {
+    my $self = shift;
+    return $self->{challenge};
+}
+
+sub keytype {
+    my $self = shift;
+    return lc( $self->{keytype} || 'rsa' );
+}
+
+1;
